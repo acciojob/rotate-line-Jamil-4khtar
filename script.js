388cc9534a74c3e87a3678bc41ac5347a6eff9ed
Jamil-4khtar <jamil4khtar@gmail.com>
2024-09-20 15:22:45 +0530

    Finished

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //your JS code here. If required.
 let line = document.getElementById('line');
 
-let angle = 2;
+let angle = 0;
 function rotateLine() {
-    line.style.transform = rotate(`${angle}deg`);
+    angle += 2;
+    line.style.transform = `rotate(${angle}deg)`;
 }
 
 setInterval(rotateLine, 20);
